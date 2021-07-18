c6ea2f08459c6953f72c38a4e0328aaab5a26ff6
David Saldarriaga <davidsaldarriaga.pardo@gmail.com>
2021-07-18 11:52:45 -0500

    Cors solved

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
